68ef8c0c124ce511ff4a652541f69b6a5113fe48
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2018-06-20 15:16:15 -0300

    Added Vaadin add-on to use grid with Spring Data data provider

--- src/main/java/crud/backend/PersonRepository.java
+++ src/main/java/crud/backend/PersonRepository.java
@@ -2,6 +2,8 @@
 package crud.backend;
 
 import java.util.List;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -20,4 +22,7 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     
     long countByNameLikeIgnoreCase(String nameFilter);
 
+    Page<Person> findByNameContainsIgnoreCase(String name, Pageable pageable);
+
+    long countByNameContainsIgnoreCase(String name);
 }
--- src/main/java/crud/vaadin/PersonListView.java
+++ src/main/java/crud/vaadin/PersonListView.java
@@ -1,18 +1,20 @@
 package crud.vaadin;
 
+import com.vaadin.data.provider.Query;
+import com.vaadin.data.provider.QuerySortOrder;
+import com.vaadin.data.provider.Sort;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
-import com.vaadin.shared.data.sort.SortDirection;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Notification;
-import com.vaadin.ui.VerticalLayout;
 import crud.backend.Person;
 import crud.backend.PersonRepository;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.vaadin.artur.spring.dataprovider.FilterablePageableDataProvider;
 import org.vaadin.spring.events.EventBus;
 import org.vaadin.spring.events.EventScope;
 import org.vaadin.spring.events.annotation.EventBusListenerMethod;
@@ -22,10 +24,8 @@ import org.vaadin.viritin.fields.MTextField;
 import org.vaadin.viritin.grid.MGrid;
 import org.vaadin.viritin.layouts.MHorizontalLayout;
 import org.vaadin.viritin.layouts.MVerticalLayout;
-import sun.plugin.util.NotifierObject;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
 @UIScope
@@ -35,9 +35,9 @@ public class PersonListView extends MVerticalLayout implements View {
 
     static final String VIEW_NAME = "persons";
 
-    PersonRepository repo;
-    PersonForm personForm;
-    EventBus.UIEventBus eventBus;
+    private PersonRepository repo;
+    private PersonForm personForm;
+    private EventBus.UIEventBus eventBus;
 
     private MGrid<Person> list = new MGrid<>(Person.class)
             .withProperties("name", "email")
@@ -53,6 +53,27 @@ public class PersonListView extends MVerticalLayout implements View {
     private Button delete = new ConfirmButton(VaadinIcons.TRASH,
             "Are you sure you want to delete the entry?", this::remove);
 
+    private FilterablePageableDataProvider<Person, Object> dataProvider = new FilterablePageableDataProvider<Person, Object>() {
+        @Override
+        protected Page<Person> fetchFromBackEnd(Query<Person, Object> query, Pageable pageable) {
+            return repo.findByNameContainsIgnoreCase(getFilter(), pageable);
+        }
+
+        private String getFilter() {
+            return getOptionalFilter().orElse("");
+        }
+
+        @Override
+        protected List<QuerySortOrder> getDefaultSortOrders() {
+            return Sort.asc("name").build();
+        }
+
+        @Override
+        protected int sizeInBackEnd(Query<Person, Object> query) {
+            return Math.toIntExact(repo.countByNameContainsIgnoreCase(getFilter()));
+        }
+    };
+
     public PersonListView(PersonRepository r, PersonForm f, EventBus.UIEventBus b) {
         this.repo = r;
         this.personForm = f;
@@ -63,95 +84,48 @@ public class PersonListView extends MVerticalLayout implements View {
     void init() {
         addComponents(new MHorizontalLayout(filterByName, addNew, edit, delete), list);
         expand(list);
-        listEntities();
+        list.setDataProvider(dataProvider);
         list.asSingleSelect().addValueChangeListener(e -> adjustActionButtonState());
-        filterByName.addValueChangeListener(e -> {
-            listEntities(e.getValue());
-        });
+        filterByName.addValueChangeListener(e -> setFilter(e.getValue()));
 
         // Listen to change events emitted by PersonForm see onEvent method
         eventBus.subscribe(this);
     }
 
-    protected void adjustActionButtonState() {
+    private void setFilter(String value) {
+        dataProvider.setFilter(value);
+        adjustActionButtonState();
+    }
+
+    private void adjustActionButtonState() {
         boolean hasSelection = !list.getSelectedItems().isEmpty();
         edit.setEnabled(hasSelection);
         delete.setEnabled(hasSelection);
     }
 
-    private void listEntities() {
-        listEntities(filterByName.getValue());
-    }
-
-    final int PAGESIZE = 45;
-
-    private void listEntities(String nameFilter) {
-        // A dead simple in memory listing would be:
-        // list.setRows(repo.findAll());
-
-        // But we want to support filtering, first add the % marks for SQL name query
-        String likeFilter = "%" + nameFilter + "%";
-
-        // If there is a moderate amount of rows, one can just fetch everything
-        //list.setRows(repo.findByNameLikeIgnoreCase(likeFilter));
-
-        // Lazy binding for better optimized connection from the Vaadin Grid to
-        // Spring Repository. This approach uses less memory and database
-        // resources. Use this approach if you expect you'll have lots of data
-        // in your table. There are simpler APIs if you don't need sorting.
-        list.setDataProvider(
-                // lazy entity fetching strategy, due to a design flaw in DataProvider API,
-                // this is bit tricky with Spring Data's Pageable abstration as requests
-                // by Grid may be on two pages, see https://github.com/vaadin/framework/issues/8982
-                // TODO see if this could be simplified in Viritin MGrid
-                (sortOrder, offset, limit) -> {
-                    final int pageSize = limit;
-                    final int startPage = (int) Math.floor((double) offset / pageSize);
-                    final int endPage = (int) Math.floor((double) (offset + pageSize - 1) / pageSize);
-                    final Sort.Direction sortDirection = sortOrder.isEmpty() || sortOrder.get(0).getDirection() == SortDirection.ASCENDING ? Sort.Direction.ASC : Sort.Direction.DESC;
-                    // fall back to id as "natural order"
-                    final String sortProperty = sortOrder.isEmpty() ? "id" : sortOrder.get(0).getSorted();
-                    if (startPage != endPage) {
-                        List<Person> page0 = repo.findByNameLikeIgnoreCase(likeFilter, PageRequest.of(startPage, pageSize, sortDirection, sortProperty));
-                        page0 = page0.subList(offset % pageSize, page0.size());
-                        List<Person> page1 = repo.findByNameLikeIgnoreCase(likeFilter, PageRequest.of(endPage, pageSize, sortDirection, sortProperty));
-                        page1 = page1.subList(0, limit - page0.size());
-                        List<Person> result = new ArrayList<>(page0);
-                        result.addAll(page1);
-                        return result.stream();
-                    } else {
-                        return repo.findByNameLikeIgnoreCase(likeFilter, PageRequest.of(startPage, pageSize, sortDirection, sortProperty)).stream();
-                    }
-                },
-                // count fetching strategy
-                () -> (int) repo.countByNameLikeIgnoreCase(likeFilter)
-        );
-        adjustActionButtonState();
-    }
-
-    public void add(Button.ClickEvent clickEvent) {
+    private void add(Button.ClickEvent clickEvent) {
         edit(new Person());
     }
 
-    public void edit(Button.ClickEvent clickEvent) {
+    private void edit(Button.ClickEvent clickEvent) {
         edit(list.asSingleSelect().getValue());
     }
 
-    public void remove() {
+    private void remove() {
         repo.delete(list.asSingleSelect().getValue());
         list.deselectAll();
         Notification.show("Person deleted", Notification.Type.WARNING_MESSAGE);
-        listEntities();
+        setFilter("");
     }
 
-    protected void edit(final Person phoneBookEntry) {
+    private void edit(final Person phoneBookEntry) {
         personForm.setEntity(phoneBookEntry);
         personForm.openInModalPopup();
     }
 
     @EventBusListenerMethod(scope = EventScope.UI)
     public void onPersonModified(PersonModifiedEvent event) {
-        listEntities();
+        setFilter("");
         personForm.closePopup();
     }
 
